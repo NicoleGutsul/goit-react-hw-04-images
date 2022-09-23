7b27e5de215f10cb472e7d42c850b4e123551389
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-09-23 21:41:17 +0100

    debug

--- src/components/FetchData/FetchData.jsx
+++ src/components/FetchData/FetchData.jsx
@@ -1,4 +1,4 @@
-const fetchData = (inputFilter, page) => {
+const FetchData = (inputFilter, page) => {
     return fetch(
       `https://pixabay.com/api/?q=${inputFilter}&page=${page}&key=30011716-3e5d547fa81f6796b48e23f2e&image_type=photo&orientation=horizontal&per_page=12`
     ).then(res => {
@@ -7,4 +7,4 @@ const fetchData = (inputFilter, page) => {
       }
     });
   };
-  export default fetchData;
+  export default FetchData;
