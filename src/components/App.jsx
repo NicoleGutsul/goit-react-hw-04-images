82654903b7c10838c917fa4b53cf8d22d3924474
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-09-23 20:01:33 +0100

    adds styles to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import { AppStyled } from "./App.styled";
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <AppStyled>
+      <>
+      </>
+    </AppStyled>
   );
 };
