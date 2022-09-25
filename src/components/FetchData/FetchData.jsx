91355af9b1aead752697fff5b2717c193f0c46f9
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-09-25 11:36:23 +0100

    changes FetchData with axios

--- src/components/FetchData/FetchData.jsx
+++ src/components/FetchData/FetchData.jsx
@@ -1,10 +1,27 @@
-const FetchData = (inputFilter, page) => {
-  return fetch(
-    `https://pixabay.com/api/?q=${inputFilter}&page=${page}&key=30011716-3e5d547fa81f6796b48e23f2e&image_type=photo&orientation=horizontal&per_page=12`
-  ).then(res => {
-    if (res.ok) {
-      return res.json();
-    }
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api';
+const API_KEY = '30011716-3e5d547fa81f6796b48e23f2e';
+
+const FetchData = async (inputFilter, page) => {
+  const searchParams = new URLSearchParams({
+    key: API_KEY,
+    q: inputFilter,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page,
+    per_page: 12,
   });
+
+  const response = await axios.get(`/?${searchParams}`);
+  return response.data.hits.map(
+    ({ id, largeImageURL, webformatURL, tags }) => ({
+      id,
+      largeImageURL,
+      webformatURL,
+      tags,
+    })
+  );
 };
 export default FetchData;
