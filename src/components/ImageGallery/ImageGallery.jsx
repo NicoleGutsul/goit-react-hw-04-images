fdbd38825857395712b5687fec4ff7576eb69cce
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-09-25 12:47:26 +0100

    changes ImageGallery with react hooks

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,113 +1,22 @@
-import { Component } from "react";
+import PropTypes from 'prop-types';
 import { GalleryImage } from "./ImageGallery.styled";
 import ImageGalleryItem from "components/ImageGalleryItem";
-import { Circles } from "react-loader-spinner";
-import { ButtonLoadMore } from "../LoadMore/LoadMore";
-import Modal from "../Modal";
-import FetchData  from "../FetchData/FetchData";
 
-export default class ImageGallery extends Component {
-    state = {
-        gallery: [],
-        page: 1,
-        loading: false,
-        modal: false,
-        currentUrl: '',
-        currentTags: '',
-        total: 0,
-        totalHits: 0,
-    };
+const ImageGallery = ({ images }) => {
 
-    componentDidUpdate(prevProps, prevState) {
-        if (prevState.page !== this.state.page) {
-            this.setState({ loading: true });
-            FetchData(this.props.inputFilter, this.state.page)
-            .then(gallery => {
-                this.setState(prevState => ({
-                    gallery: [...prevState.gallery, ...gallery.hits],
-                    loading: false,
-                    total: prevState.total + gallery.hits.length,
-                    totalHits: gallery.totalHits,
-                }));
-            })
-            .catch(error => this.setState({ error }));
-        }
-        if (prevProps.inputFilter !== this.props.inputFilter) {
-            this.setState({ loading: true, gallery: [], page: 1, total: 0 });
-            FetchData(this.props.inputFilter, this.state.page)
-              .then(gallery => {
-                this.setState(prevState => ({
-                    gallery: [...prevState.gallery, ...gallery.hits],
-                    loading: false,
-                    total: prevState.total + gallery.hits.length,
-                    totalHits: gallery.totalHits,
-                }));
-              })
-              .catch(error => this.setState({ error }));
-        }
-    }
-
-   
-
-    getImage = (webformatURL, tags) => {
-        this.setState({ currentUrl: webformatURL, currentTags: tags});
-    }
-
-    loadMore = () => {
-        this.setState(prevState => ({ page: prevState.page + 1 }));
-    };
-
-    render() {
-        return (
-            <GalleryImage>
-                {this.state.loading && (
-                    <Circles
-                       height="80"
-                       width="80"
-                       color="#4fa94d"
-                       ariaLabel="circles-loading"
-                       wrapperStyle={{}}
-                       wrapperClass=""
-                       visible={true}
-                       />
-                )}
-
-                {this.state.gallery.total === 0 ? (
-                    <p>
-                        Maybe u made miss? We didn't find photos with this name {this.props.inputFilter}
-                    </p>
-                ) : null}
-
-                {this.state.gallery === '' ? (
-                    <p>Enter name images!</p>
-                ) : (
-                    <ImageGalleryItem 
-                       gallery={this.state.gallery}
-                       onClickModal={this.toggleModal}
-                       getImage={this.getImage}
-                    />
-                )}
-
-                {(this.state.gallery !== '') &
-                (this.state.gallery.total !== 0) &
-                (this.state.totalHits !== this.state.total) ? (
-                <ButtonLoadMore 
-                   onClick={this.loadMore} 
-                />
-                ) : (
-                ''
-                )}
-
-                {this.state.modal && (
-                    <Modal onClose={this.toggleModal}>
-                       {/* Url={this.state.currentUrl}
-                       Tags={this.state.currentTags} */}
-                       <img src={this.state.currentUrl} alt={this.state.currentTags}/>
-                    </Modal>
-                    
-                )}
-
+   return (
+        <GalleryImage>
+          {images.map((image, index) => {
+              return  <ImageGalleryItem  image={image} key={index}/>;
+           })}  
             </GalleryImage>
-        )
-    }
-}
+    );
+};
+
+ImageGallery.propTypes = {
+    images: PropTypes.array,
+    image: PropTypes.shape({
+      key: PropTypes.number.isRequired,
+    }),
+  };
+  export default ImageGallery;
